dab7415126a14078955e545853f1fde0f7247485
Mikhail Shardin <kriptonit1988@gmail.com>
2019-05-21 20:27:12 +0500

    Первые компоненты

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = () => <h1>My Todo List</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Search = () => <input placeholder="search" />;
+
+const TodoList = () => (
+  <ul>
+    <li>Learn React</li>
+    <li>Learn Redux</li>
+    <li>Build Todo App</li>
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <Header />
+    <Search />
+    <TodoList />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
