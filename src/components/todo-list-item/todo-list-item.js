476b5b19628f7ba6424bb876970b30820c6f5787
Mikhail Shardin <kriptonit1988@gmail.com>
2019-05-24 17:11:42 +0500

    Изменение state с помощью setState при событии

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,12 +3,22 @@ import React, { Component } from "react";
 import "./todo-list-item.css";
 
 class TodoListItem extends Component {
+  state = {
+    done: false,
+  }
+
   onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`);
+    const { done } = this.state;
+    this.setState({
+      done: !done,
+    });
   }
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    const labelClassName = `todo-list-item${done ? ` done` : ``}`;
 
     const style = {
       color: important ? "steelblue" : "black",
@@ -16,9 +26,9 @@ class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={labelClassName}>
         <span
-          className="todo-list-item-label"
+          className='todo-list-item-label'
           style={style}
           onClick={this.onLabelClick}
         >
