5bb5ceaf9ef8733720a22b39cc6b8688bab3fae8
Mikhail Shardin <kriptonit1988@gmail.com>
2019-05-25 14:05:11 +0500

    Updates

---
+++ precache-manifest.3d2ec2d68aaccde3ae1d275c94338357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40d90a8fd185c3c78f7bd023e291262",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "125f5601133766d05574",
+    "url": "/todo-react/static/css/main.e78c1b59.chunk.css"
+  },
+  {
+    "revision": "cd5d81d4826c04a8536b",
+    "url": "/todo-react/static/js/2.30340e50.chunk.js"
+  },
+  {
+    "revision": "125f5601133766d05574",
+    "url": "/todo-react/static/js/main.987d2395.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  }
+]);
